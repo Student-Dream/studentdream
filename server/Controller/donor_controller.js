65d476963ee31f306074160be8913018e201c753
SanadHantash <sanadhantash24@gmail.com>
2023-11-20 23:51:29 +0300

    project done

--- server/Controller/donor_controller.js
+++ server/Controller/donor_controller.js
@@ -67,7 +67,8 @@ exports.createCheckoutSession = async (req, res) => {
       success_url: 'http://localhost:3000/success',
       cancel_url: 'http://localhost:3000/cancel',
     });
-
+    
+    await postHistory(req, res);
     res.json({ id: session.id });
     
      await Request.checkconfirm(donation.userId);
